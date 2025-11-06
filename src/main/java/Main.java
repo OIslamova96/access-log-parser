c668c66f350aca5623bc232f501359f71e725829
Olga Islamova <olga.islamowa96@yandex.ru>
2025-11-06 13:32:41 +0300

    Добавлен расчет и вывод в консоль суммы,разности,произведения и частного

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Введите текст и нажмите <Enter>:");
         Scanner scanner = new Scanner(System.in);
-        String inputText = scanner.nextLine();
-        System.out.println("Длина введенного текста: " + inputText.length());
+        System.out.print("Введите первое целое число и нажмите <Enter>: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Введите второе целое число и нажмите <Enter>: ");
+        int num2 = scanner.nextInt();
+        int sum = num1 + num2;
+        System.out.println("Сумма: " + sum);
+        int difference = num1 - num2;
+        System.out.println("Разность: " + difference);
+        int product = num1 * num2;
+        System.out.println("Произведение: " + product);
+        if (num2 != 0) {
+            double quotient = (double) num1 / num2;
+            System.out.println("Частное: " + quotient);
+        } else {
+            System.out.println("Деление на ноль невозможно.");
+        }
         scanner.close();
     }
 }
