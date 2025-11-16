5888e34a8d8864a076cb138ae9cd05cf350f24aa
Olga Islamova <olga.islamowa96@yandex.ru>
2025-11-16 20:33:51 +0300

    изменена логика метода main добавлен бесконечный цикл запросов пути к файлу

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,29 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите первое целое число и нажмите <Enter>: ");
-        int num1 = scanner.nextInt();
-        System.out.print("Введите второе целое число и нажмите <Enter>: ");
-        int num2 = scanner.nextInt();
-        int sum = num1 + num2;
-        System.out.println("Сумма: " + sum);
-        int difference = num1 - num2;
-        System.out.println("Разность: " + difference);
-        int product = num1 * num2;
-        System.out.println("Произведение: " + product);
-        if (num2 != 0) {
-            double quotient = (double) num1 / num2;
-            System.out.println("Частное: " + quotient);
-        } else {
-            System.out.println("Деление на ноль невозможно.");
+        int validFileCount = 0;
+        while (true) {
+            System.out.println("Введите путь к файлу для парсинга:");
+            String filePath = scanner.nextLine();
+            File file = new File(filePath);
+            boolean fileExists = file.exists();
+            boolean isActualFile = file.isFile();
+            if (!fileExists) {
+                System.out.println("Файл или директория по указанному пути не существует. Попробуйте еще раз.");
+                continue;
+            }
+            if (!isActualFile) {
+                System.out.println("Указанный путь ведет к папке, а не к файлу. Попробуйте еще раз.");
+                continue;
+            }
+            System.out.println("Путь указан верно");
+            validFileCount++;
+            System.out.println("Это файл номер " + validFileCount);
         }
-        scanner.close();
     }
 }
+
